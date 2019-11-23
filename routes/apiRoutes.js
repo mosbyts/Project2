59c290806b3824a3c76ac6088b021e2e507ba2fe
Tianna Mosby <mosbyts@vcu.edu>
2019-11-23 14:06:15 -0500

    Integrated the movie watchlist portion.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,9 +7,12 @@
 
 // Requiring our Todo model
 var db = require("../models");
+var Watch = require("../models/watch.js");
 
 // Routes
 // =============================================================
+
+//Movie review post API call start
 module.exports = function(app) {
   // GET route for getting all of the reviews
   app.get("/api/posts/", function(req, res) {
@@ -74,82 +77,37 @@ module.exports = function(app) {
     });
   });
 };
+//Movie review post API call end
 
-/*
-var db = require("../models");
 
-// Routes
+//Movie watch list API call start
 // =============================================================
 module.exports = function(app) {
 
-  // GET route for getting all of the posts
-  app.get("/api/posts/", function(req, res) {
-    db.Post.findAll({})
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
+  // Get all watchs
+  app.get("/api/all", function(req, res) {
+
+    // Finding all watchs, and then returning them to the user as JSON.
+    // Sequelize queries are asynchronous, which helps with perceived speed.
+    // If we want something to be guaranteed to happen after the query, we'll use
+    // the .then function
+    Watch.findAll({}).then(function(results) {
+      // results are available to us inside the .then
+      res.json(results);
+    });
+
   });
 
-  // Get route for returning posts of a specific category
-  app.get("/api/posts/category/:category", function(req, res) {
-    db.Post.findAll({
-      where: {
-        category: req.params.category
-      }
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-  // Get route for retrieving a single post
-  app.get("/api/posts/:id", function(req, res) {
-    db.Post.findOne({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-  // POST route for saving a new post
-  app.post("/api/posts", function(req, res) {
-    console.log(req.body);
-    db.Post.create({
+  // Add a watch
+  app.post("/api/new", function(req, res) {
+    Watch.create({
       title: req.body.title,
       body: req.body.body,
-      category: req.body.category
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-  // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function(req, res) {
-    db.Post.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-  // PUT route for updating posts
-  app.put("/api/posts", function(req, res) {
-    db.Post.update(req.body,
-      {
-        where: {
-          id: req.body.id
-        }
-      })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
+      created_at: req.body.created_at
+    }).then(function(results) {
+      // `results` here would be the newly created Watch
+      res.end();
+    });
   });
 };
-
+//Movie watch list API call end
--- server.js
+++ server.js
@@ -28,41 +28,8 @@ require("./routes/htmlRoutes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: false }).then(function(){
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
-
-/*require("dotenv").config();
-var express = require("express");
-
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-// Requiring our models for syncing
-var db = require("./models");
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Static directory
-app.use(express.static("public"));
-
-// Routes
-// =============================================================
-require("./routes/apiRoutes.js")(app);
-require("./routes/htmlRoutes.js")(app);
-
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: false }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-});
-module.exports = app;
-*/
