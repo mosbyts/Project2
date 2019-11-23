4346ed7e97374c351e3c2e7edef195015aa4b907
Tayvon Pierre Randall <PierreMac@VCUPierre.local>
2019-11-23 17:44:32 -0500

    completed search by genre functionality

--- public/js/home.js
+++ public/js/home.js
@@ -12,6 +12,11 @@ $(document).ready(function(){
     getPopularMovies();
     $("#userSearchDiv").hide();
     $("#genrePage").hide();
+
+    $(".flip-card").on("click", function(){
+        var catID = $(this).attr("value");
+        getMoviesByCategory(catID);
+    })
 });
 
 //Search movies by name click function
@@ -24,7 +29,7 @@ $("#searchMoviesBtn").on("click", function(event){
     $("#availableOn").empty();
 });
 
-function getMovieStreamDetails(movieTitle, action){
+function getMovieStreamDetails(movieTitle, action, movieData){
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -65,12 +70,64 @@ function getMovieStreamDetails(movieTitle, action){
             $("#availableOn").append(streamingP);
         });
     } else if (action === 'category') {
-        $.ajax(settings).done(function (response) {
+        $.ajax(settings).then(function (response) {
             let streamingLocations = [];
-            for (let i = 0; i < response.results[0].locations.length; i++){
-                streamingLocations.push(response.results[0].locations[i].display_name);
-            }   
-            return streamingLocations;
+            let moviePosterBaseUrl = "http://image.tmdb.org/t/p/w185/";
+            let tempObject;
+            //console.log("response");
+            //console.log(response);
+            if (response.results[0]){
+                for (let i = 0; i < response.results[0].locations.length; i++){
+                    streamingLocations.push(response.results[0].locations[i].display_name);
+                }
+                tempObject = {
+                    title: movieData.title,
+                    poster: moviePosterBaseUrl + movieData.poster_path,
+                    releaseDate: movieData.release_date,
+                    overview: movieData.overview,
+                    streamingLocations: streamingLocations,
+                    genreID: movieData.genre_ids[0]
+                }  
+            } else {
+                //console.log("location not found");
+                tempObject = {
+                    title: movieData.title,
+                    poster: moviePosterBaseUrl + movieData.poster_path,
+                    releaseDate: movieData.release_date,
+                    overview: movieData.overview,
+                    streamingLocations: "Not Currently Streaming",
+                    genreID: movieData.genre_ids[0]
+                }  
+            }
+            //console.log("test");
+            //console.log(movieData);
+             
+            // console.log("tempObject");
+            console.log(tempObject);
+            let divholder = $("<div>", {id: "result1"});
+            let moviePoster = $("<img>", {src: tempObject.poster, class: "searchedMovieImg text-center"});
+            let titleP = $("<p>");
+            titleP.append(tempObject.title);
+            let streamingP = $("<p>");
+            for (let i = 0; i < tempObject.streamingLocations.length; i++){
+                if (i >= 1){
+                    streamingP.append(", ");
+                    streamingP.append(tempObject.streamingLocations[i]);
+                } else {
+                    streamingP.append(tempObject.streamingLocations[i]);
+                }
+            }
+            divholder.append(titleP);
+            divholder.append(moviePoster);
+            divholder.appendTo("#movieGenreDisplay");
+            // if (localStorage.getItem("movieCat"+tempObject.genreID)){
+            //     var array = [];
+            //     array.push(localStorage.getItem("movieCat"+tempObject.genreID));
+            //     localStorage.setItem("movieCat"+tempObject.genreID, JSON.stringify(array));
+            // } else {
+            //     localStorage.setItem("movieCat"+tempObject.genreID, JSON.stringify(tempObject));
+            // }
+            //return tempObject;
         })
     } else {
         //defualt 
@@ -78,12 +135,12 @@ function getMovieStreamDetails(movieTitle, action){
 }
 
 //Search movies by genre click function
-$("#actionCard").on("click", function(event){
-    event.preventDefault();
-    $("#homePage").hide();
-    $("#genrePage").show();
-    getMovieGenres();
-});
+// $("#actionCard").on("click", function(event){
+//     event.preventDefault();
+    // $("#homePage").hide();
+    // $("#genrePage").show();
+//     getMovieGenres();
+// });
 
 function getMovieGenres(){
     let key = "5f7135150c434e2b62be14b37e1617f5";
@@ -95,42 +152,46 @@ function getMovieGenres(){
     });
 }
 
-function getMoviesByCategory(){
+function getMoviesByCategory(catID){
     let key = "5f7135150c434e2b62be14b37e1617f5";
-    let movieGenreArray = [28, 16, 35, 99, 14, 27, 9648, 10749, 878, 53];
-    let movieObjects = [];
-    let moviePosterBaseUrl = "http://image.tmdb.org/t/p/w185/";
+    // movieGenreArray = [28, 16, 35, 99, 14, 27, 9648, 10749, 878, 53];
+    //let movieObjects = [];
     // example http://image.tmdb.org/t/p/w185//kvpNZAQow5es1tSY6XW2jAZuPPG.jpg ... it does require the double backspace
     
-    for (let i = 0; i < movieGenreArray.length; i++){
-        let queryString = "https://api.themoviedb.org/3/discover/movie?api_key="+key+"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres="+movieGenreArray[i];
+    //for (let i = 0; i < movieGenreArray.length; i++){
+        //let queryString = "https://api.themoviedb.org/3/discover/movie?api_key="+key+"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres="+catID;
+        let queryString = "https://api.themoviedb.org/3/discover/movie?api_key="+key+"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2018&with_release_type=4%7C5%7C6&with_genres="+catID;
         $.get(queryString, function(results){
             console.log("*** Movies by Genre search using tMDB api***");
-            console.log(results);
-            for (let i = 0; i < 10; i++){
-                var streaming = getMovieStreamDetails(results.results[i].title ,"category");
-                let tempObject = {
-                    title: results.results[i].title,
-                    poster: moviePosterBaseUrl + results.results[i].poster_path,
-                    releaseDate: results.results[i].release_date,
-                    overview: results.results[i].overview,
-                    streamingLocations: streaming,
-                    genreID: results.results[i].genre_ids[0]
-                }
-                movieObjects.push(tempObject);
+            console.log("Category id:"+catID);
+            //console.log(results);
+            for (let i = 0; i < 20; i++){
+                getMovieStreamDetails(results.results[i].title ,"category", results.results[i]);
+                // let tempObject = {
+                //     title: results.results[i].title,
+                //     poster: moviePosterBaseUrl + results.results[i].poster_path,
+                //     releaseDate: results.results[i].release_date,
+                //     overview: results.results[i].overview,
+                //     streamingLocations: streaming,
+                //     genreID: results.results[i].genre_ids[0]
+                // }
+                //movieObjects.push(streaming);
                 //moviePosters.push(results.results[i].poster_path);
             }
-            console.log("movies for category - "+ movieGenreArray[i]);
-            console.log(movieObjects);
+            //console.log("movies for category - "+ movieGenreArray[i]);
             //console.log("**Movie Posters links**")
             //console.log(moviePosters);
+        }).then(function(){
+            $("#homePage").hide();
+            $("#genrePage").show();
         });
-    }
+    //}
 }
 
 function getPopularMovies(){
     let key = "5f7135150c434e2b62be14b37e1617f5";
-    let queryString = "https://api.themoviedb.org/3/discover/movie?api_key="+key+"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+    //let queryString = "https://api.themoviedb.org/3/discover/movie?api_key="+key+"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+    let queryString = "https://api.themoviedb.org/3/discover/movie?api_key="+key+"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_release_type=4%7C5%7C6&without_genres=80"
     let popularMoviesObject = [];
     let moviePosterBaseUrl = "http://image.tmdb.org/t/p/w185/";
     // example http://image.tmdb.org/t/p/w185//kvpNZAQow5es1tSY6XW2jAZuPPG.jpg ... it does require the double backspace
