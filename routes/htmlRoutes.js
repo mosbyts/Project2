40e96c73d2d1380b22bf827ed0a2a82b6c1216b0
Tianna Mosby <mosbyts@vcu.edu>
2019-11-24 20:38:41 -0500

    Updated some styling and changed the way files are linked in the HTML.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -23,6 +23,10 @@ module.exports = function(app) {
   app.get("/main", function(req, res) {
     res.sendFile(path.join(__dirname, "../views/layouts/main.html"));
   });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/layouts/main.html"));
+  });
 };
 
 /*
