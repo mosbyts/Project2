57355f21763789763d887e73b5ba30f8bf27ac7e
Tianna Mosby <mosbyts@vcu.edu>
2019-12-27 19:16:26 -0500

    Included JawsDB.

--- config/connection.js
+++ config/connection.js
@@ -6,16 +6,20 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("projectTwo", "root", "password", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var sequelize = new Sequelize("projectTwo", "root", "password", {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  })
+};
 
 // Exports the connection for other files to use
 module.exports = sequelize;
